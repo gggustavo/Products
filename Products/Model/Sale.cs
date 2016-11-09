3335903f444c45b1f87ccfac6b1143e8b0729ca0
gggustavo <ggustavo@icloud.com>
2016-11-08 22:05:27 -0300

    add sale

---
+++ Products/Model/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Sale
+    {
+        public int IdSale { get; set; }
+        public DateTime DateSale { get; set; }
+                        
+        public ICollection<Product> Products  { get; set; }
+
+
+
+    }
+}
